4c0e35b5394286a1517f354cf2197a6de31cca1a
pravin8470 <kumar.pravind@gmail.com>
2023-01-09 01:57:27 +0530

    End Of Section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -12,8 +12,8 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        
-         [JsonConverter(typeof(DateOnlyJsonConverter))]
+
+        [JsonConverter(typeof(DateOnlyJsonConverter))]
         public DateOnly DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
@@ -26,11 +26,8 @@ namespace API.Entities
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new();
 
-
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
 
 
     }
